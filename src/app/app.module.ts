00441768076008227d63546012d0e532aed46c67
victor <vrodriguess@indracompany.com>
2021-08-25 14:56:50 -0300

    criação dos componentes, serviços e rotas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicialComponent } from './componentes/inicial/inicial.component';
+import { ProdutoComponent } from './componentes/produto/produto.component';
+import { CarrinhoComponent } from './componentes/carrinho/carrinho.component';
+import { CheckoutComponent } from './componentes/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicialComponent,
+    ProdutoComponent,
+    CarrinhoComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
